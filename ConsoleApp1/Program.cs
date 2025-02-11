5951b9f724e32dc9ec6c1ee5d22e8e97b4a75292
Guilherme Teixeira <141333177+Guilhermetxa@users.noreply.github.com>
2025-02-11 10:27:27 -0300

    Aula dia 11-02-2025 - C#
    Código exemplo 1 em C#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 18;
+
+
+            Console.WriteLine(idade);
+
+            if(idade >= 18)
+            {
+                Console.WriteLine("Parabéns");
+            }
+            else
+            {
+                //Console.WriteLine("Vc tem somente" + idade + "anos");
+                Console.WriteLine($"Vc tem somente {idade} anos");
+            }
+
+            //Carro carro02 = new();
+
+            Carro carro = new Carro();
+            carro.Modelo = "Fusca";
+            carro.Ano = 1988;
+            carro.Velocidade = 40;
+
+        }
+    }
+}
